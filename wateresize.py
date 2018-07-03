387a496adc832991d96116ccbc373a655b6032ac
mrt0rtikize <i_am@rogov.al>
2018-07-03 16:16:27 +0300

    change 'print' to logging

--- wateresize.py
+++ wateresize.py
@@ -8,14 +8,19 @@ from os import listdir, mkdir
 from os.path import isfile, join, exists, exists
 from wand.image import Image
 import argparse
+import logging
 
+
+# logging
+logging.basicConfig(filename="wateresize.log", level=logging.DEBUG)
 
 # ===============================
 # Check if directory exists
 # =============================== 
 def check_in_path(path):
     if not exists(path):
-        raise "{0} directory does not exists.".format(path) 
+        logging.error("{0} directory does not exists.".format(path))
+        # raise "{0} directory does not exists.".format(path) 
     return path
 
 # ===============================
@@ -23,9 +28,9 @@ def check_in_path(path):
 # =============================== 
 def create_out_path(out):
     if not exists(out):
-        print("Creating directory {0}.".format(out))
+        logging.info("Creating directory {0}.".format(out))
         mkdir(out)
-        print("Created directory {0}.".format(out))
+        logging.info("Created directory {0}.".format(out))
     return out
 
 # ===============================
@@ -33,7 +38,8 @@ def create_out_path(out):
 # ===============================         
 def check_resize_value(value):
     if (value < 5):
-        raise NameError("Invalid resize value: " + value)
+        logging.error("Invalid resize value: " + value) 
+        # raise NameError("Invalid resize value: " + value)
     return str(value) + '%'
 
 # ===============================
@@ -43,24 +49,24 @@ def orientation(img):
     for k, v in img.metadata.items():
         if k.startswith("exif:Orientation"):
             return v
-            
+
 # ===============================
 # Rotate if necessary and set watermark
 # =============================== 
 def watermark_position(img, watermark):
     orient = orientation(img)
-    print("orientation = {0}".format(orient))
+    logging.info("orientation = {0}".format(orient))
     if (orient == "8"):
         img.rotate(-90)
         set_watermark(img, watermark)
         img.rotate(90)
     elif (orient == "6"):
         img.rotate(90)
-        set_watermark(img, watermark)        
+        set_watermark(img, watermark)
         img.rotate(-90)
     else:
         set_watermark(img, watermark)
-                    
+
 # ===============================
 # Set watermark at the bottom-left corner of the image
 # ===============================                     
@@ -88,24 +94,23 @@ if args.w:
     watermark = Image(filename=args.w)
 
 # Run
-print("Resizing all files from {0}. Output to {1}".format(in_path, out_path))
+logging.info("Resizing all files from {0}. Output to {1}".format(in_path, out_path))
 for f in listdir(in_path):
     filename=join(in_path, f)
     if isfile(filename):
-        print("---\n" + filename)
+        logging.info(filename)
         try:
             with Image(filename=filename) as img:
-                print("Size before {0}.".format(img.size))
+                logging.debug("Size before {0}.".format(img.size))
                 if args.r:
                     img.transform(resize = resize_value)
                 if args.w:
-                    watermark_position(img, watermark) 
+                    watermark_position(img, watermark)
                 img.save(filename = out_path + "/" + f)
-                print("Size after {0}.".format(img.size))
+                logging.debug("Size after {0}.".format(img.size))
         except Exception as e:
-            print("Image processing error:", e)
+            logging.error("Image processing error:", e)
 
-print("Finished processing images.")
 """
 END
 """
